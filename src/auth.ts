57d9a7ef3937e6c80f0d4b1f3f894c1fc0e145c8
sinhakhyati21 <sinhakhyati21@gmail.com>
2026-06-23 12:50:00 +0530

    added github Oauth

---
+++ src/auth.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+import { connectDB } from "../src/lib/db";
+import User from "../src/models/User.model";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ user, profile }) {
+      try {
+        await connectDB();
+
+        await User.findOneAndUpdate(
+          {
+            email: user.email,
+          },
+          {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            githubUsername: (profile as any)?.login,
+          },
+          {
+            upsert: true,
+            new: true,
+          }
+        );
+
+        return true;
+      } catch (error) {
+        console.error("Error saving user:", error);
+        return false;
+      }
+    },
+  },
+
+  secret: process.env.AUTH_SECRET,
+});
